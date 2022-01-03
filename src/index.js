ac733b75bd02bb178408219e947ed5d4f3055017
Vasilisa Shokhava <vasilisa.shokhava@wolterskluwer.com>
2022-01-04 00:34:38 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
+    const converter = require('number-to-words');
+    const converted = converter.toWords(number);
+    const convertedWithoutdash = converted.replace('-', ' ');
+    return convertedWithoutdash; 
+    
 }
