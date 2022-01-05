8753dd846df088eb2f86c1e59799eb780eda1960
Vasilisa Shokhava <vasilisa.shokhava@wolterskluwer.com>
2022-01-06 00:01:42 +0300

    feat: update solution withput package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
+const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 module.exports = function toReadable (number) {
-    const converter = require('number-to-words');
-    const converted = converter.toWords(number);
-    const convertedWithoutdash = converted.replace('-', ' ');
-    return convertedWithoutdash; 
+
+
+    if (number < 20) {
+        return num[number];
+    };
+    const digit = number%10;
+    if (number < 100) {
+        const withTens = tens[~~(number/10)-2] + (digit? "-" + num[digit]: "")
+        const tensWithoutDash = withTens.replace("-", ' ');
+        return tensWithoutDash;
+    }
+    if (number < 1000) {
+        const withHundreds = num[~~(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+        const hundredsWithoutDash = withHundreds.replace("-", " ");
+        return hundredsWithoutDash;
+    }
+    if (number > 1000) {
+
+        const withThousands = toReadable(~~(number/1000)) + " thousand" + (number%1000 != 0? " " + toReadable(number%1000): "");
+        const thousandsWithoutDash = withThousands.replace("-", " ");
+        return thousandsWithoutDash;
+    }
+
     
 }
